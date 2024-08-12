fbfe33501979a5e35fe0501bf21b05ed09b00c7f
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-08-12 22:15:45 +0700

    header

--- FE/broplus/src/pages/user/theme/Header/index.js
+++ FE/broplus/src/pages/user/theme/Header/index.js
@@ -1,12 +1,43 @@
 import { memo } from "react";
 import "./style.scss";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header-top">
-          <div className="container">Header</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Trái</div>
+          <div className="col-6 header_top_right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <FaSquareFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaSquareFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaSquareFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaSquareFacebook />
+                </Link>
+                <span>Đăng nhập</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default memo(Header);
