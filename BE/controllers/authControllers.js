6bf8a47fb112fc6374266551c575ffca40f98c1a
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-09-07 16:20:16 +0700

    token

--- BE/controllers/authControllers.js
+++ BE/controllers/authControllers.js
@@ -1,69 +1,77 @@
-const User = require("../models/User");
-const brcypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+  const User = require("../models/User");
+  const brcypt = require("bcrypt");
+  const jwt = require("jsonwebtoken");
 
-const authControllers = {
-  //REGISTER
-  registerUser: async (req, res) => {
-    try {
-      const salt = await brcypt.genSalt(10);
-      const hashed = await brcypt.hash(req.body.password, salt);
+  const authControllers = {
+    //REGISTER
+    registerUser: async (req, res) => {
+      try {
+        const salt = await brcypt.genSalt(10);
+        const hashed = await brcypt.hash(req.body.password, salt);
 
-      //Create a new user
-      const newUser = new User({
-        username: req.body.username,
-        phone: req.body.phone,
-        password: hashed,
+        //Create a new user
+        const newUser = new User({
+          username: req.body.username,
+          phone: req.body.phone,
+          password: hashed,
+        });
+
+        //Save user and respond
+        const user = await newUser.save();
+        res.status(200).json(user);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+    },
+    //GENERATE NEW ACCESS TOKEN
+    generateAccessToken: (user) => {
+      return jwt.sign({ id: user._id, admin: user.admin }, process.env.MYSECRET, {
+        expiresIn: "5d",
       });
+    },
 
-      //Save user and respond
-      const user = await newUser.save();
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  },
-  //GENERATE NEW ACCESS TOKEN
-  generateAccessToken: (user) => {
-    return jwt.sign({ id: user._id, admin: user.admin }, process.env.MYSECRET, {
-      expiresIn: "5d",
-    });
-  },
+    //GENERATE NEW REFRESH TOKEN
 
-  //GENERATE NEW REFRESH TOKEN
-
-  generateRefreshToken: (user) => {
-    return jwt.sign(
-      { id: user._id, admin: user.admin },
-      process.env.MYREFRESHSECRET,
-      { expiresIn: "365d" }
-    );
-  },
-
-  //   LOGIN
-  loginUser: async (req, res) => {
-    try {
-      const user = await User.findOne({ phone: req.body.phone });
-      if (!user) {
-        res.status(404).json({ message: "User not found" });
-      }
-      const validPassword = await brcypt.compare(
-        req.body.password,
-        user.password
+    generateRefreshToken: (user) => {
+      return jwt.sign(
+        { id: user._id, admin: user.admin },
+        process.env.MYREFRESHSECRET,
+        { expiresIn: "365d" }
       );
-      if (!validPassword) {
-        res.status(404).json({ message: "Wrong password" });
-      }
-      if (user && validPassword) {
-        const accessToken = authControllers.generateAccessToken(user);
-        const refreshToken = authControllers.generateRefreshToken(user);
-        const { password, ...info } = user._doc;
-        res.status(200).json({ ...info, accessToken, refreshToken });
-      }
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  },
-};
+    },
 
-module.exports = authControllers;
+    //   LOGIN
+    loginUser: async (req, res) => {
+      try {
+        const user = await User.findOne({ phone: req.body.phone });
+        if (!user) {
+          res.status(404).json({ message: "User not found" });
+        }
+        const validPassword = await brcypt.compare(
+          req.body.password,
+          user.password
+        );
+        if (!validPassword) {
+          res.status(404).json({ message: "Wrong password" });
+        }
+        if (user && validPassword) {
+          const accessToken = authControllers.generateAccessToken(user);
+          const refreshToken = authControllers.generateRefreshToken(user);
+          res.cookie("refreshtoken", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            path: "/",
+          });
+          const { password, ...info } = user._doc;
+          res.status(200).json({ ...info, accessToken });
+        }
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+    },
+  };
+
+  //STORE TOKEN
+
+  module.exports = authControllers;
